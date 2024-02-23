ef1bbf01e2ab7263f8efb429629ea5d67321a622
Dawid Bieńkowski <dbienkowski@op.pl>
2024-02-23 01:24:24 +0100

    Adding data seeder
    Added data seeder that loads the database with initial values

--- JobBoardAPI/Program.cs
+++ JobBoardAPI/Program.cs
@@ -1,12 +1,21 @@
+using JobBoardAPI.Entities;
+using JobBoardAPI.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<JobBoardDbContext>();
+builder.Services.AddScoped<DataSeeder>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+var scope = app.Services.CreateScope();
+var seeder = scope.ServiceProvider.GetRequiredService<DataSeeder>();
+seeder.Seed();
 
 app.UseHttpsRedirection();
 
---
+++ JobBoardAPI/Services/DataSeeder.cs
@@ -0,0 +1,43 @@
+﻿using JobBoardAPI.Entities;
+
+namespace JobBoardAPI.Services
+{
+    public class DataSeeder
+    {
+        private readonly JobBoardDbContext _dbContext;
+
+        public DataSeeder(JobBoardDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public void Seed()
+        {
+            if (_dbContext.Database.CanConnect())
+            {
+                if (!_dbContext.Categories.Any())
+                {
+                    var categories = GetCategories();
+                    _dbContext.AddRange(categories);
+                    _dbContext.SaveChanges();
+                }
+            }
+        }
+
+        private IEnumerable<Category> GetCategories()
+        {
+            string[] names =
+                ["Accounting", "Construction", "Delivery", "Education", "Gastronomy", "IT", "Logistics", "Mechanics", "Production", "Sales"];
+            var categories = new List<Category>();
+
+            foreach (var name in names)
+            {
+                categories.Add(new Category()
+                {
+                    Name = name
+                });
+            }
+
+            return categories;
+        }
+    }
+}
