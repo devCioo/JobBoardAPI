eed60b61af10e280ac9614e24fec604c678ac7b0
Dawid Bieńkowski <dbienkowski@op.pl>
2024-02-23 01:04:28 +0100

    Adding database context
    Added database context and made first migration to the database

---
+++ JobBoardAPI/Entities/JobBoardDbContext.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace JobBoardAPI.Entities
+{
+    public class JobBoardDbContext : DbContext
+    {
+        private string _connectionString = "Server=(localdb)\\mssqllocaldb;Database=JobBoardDb;Trusted_Connection=True;";
+        public DbSet<JobAdvertisement> JobAdvertisements { get; set; }
+        public DbSet<JobApplication> JobApplications { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // JobAdvertisement
+            modelBuilder.Entity<JobAdvertisement>()
+                .Property(ja => ja.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+            modelBuilder.Entity<JobAdvertisement>()
+                .Property(ja => ja.CompanyName)
+                .IsRequired()
+                .HasMaxLength(70);
+            modelBuilder.Entity<JobAdvertisement>()
+                .Property(ja => ja.Responsibilities)
+                .IsRequired();
+
+            // JobApplication
+            modelBuilder.Entity<JobApplication>()
+                .Property(ja => ja.CoverLetter)
+                .IsRequired();
+
+            // Category
+            modelBuilder.Entity<Category>()
+                .Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(35);
+
+            // Address
+            modelBuilder.Entity<Address>()
+                .Property(a => a.City)
+                .IsRequired()
+                .HasMaxLength(40);
+            modelBuilder.Entity<Address>()
+                .Property(a => a.Street)
+                .IsRequired()
+                .HasMaxLength(60);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+    }
+}
---
+++ JobBoardAPI/Migrations/20240223000022_Init.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobBoardAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Addresses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    City = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: false),
+                    Street = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    PostalCode = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(35)", maxLength: 35, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "JobAdvertisements",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    CompanyName = table.Column<string>(type: "nvarchar(70)", maxLength: 70, nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false),
+                    AddressId = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Responsibilities = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Requirements = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PostedOn = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JobAdvertisements", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_JobAdvertisements_Addresses_AddressId",
+                        column: x => x.AddressId,
+                        principalTable: "Addresses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_JobAdvertisements_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "JobApplications",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CoverLetter = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AppliedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    JobAdvertisementId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JobApplications", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_JobApplications_JobAdvertisements_JobAdvertisementId",
+                        column: x => x.JobAdvertisementId,
+                        principalTable: "JobAdvertisements",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JobAdvertisements_AddressId",
+                table: "JobAdvertisements",
+                column: "AddressId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JobAdvertisements_CategoryId",
+                table: "JobAdvertisements",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JobApplications_JobAdvertisementId",
+                table: "JobApplications",
+                column: "JobAdvertisementId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "JobApplications");
+
+            migrationBuilder.DropTable(
+                name: "JobAdvertisements");
+
+            migrationBuilder.DropTable(
+                name: "Addresses");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
